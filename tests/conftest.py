40b06d05e747c9772b42990161b6c536b9a52bea
Evan Haas <evan@lagerdata.com>
2021-01-15 16:28:14 -0800

    canbus up/down + async

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 import lager_cantools
 import can
+from lager import lager
 
 @pytest.fixture(scope='session')
 def flash_bootloader(request, fail_bootloader):
     if fail_bootloader:
-        pytest.fail('boatloader failed')
+        pytest.fail('bootloader failed')
         # or the following 2 lines
         # fail_marker = pytest.mark.skip(reason="bootloader failed")
         # request.node.add_marker(fail_marker)
@@ -17,7 +18,36 @@ def flash_bootloader(request, fail_bootloader):
 def dbc():
     yield lager_cantools.database.load_file('canconv.dbc', strict=False)
 
+@pytest.fixture(scope='session')
+def canbus_bitrate():
+    yield 500000
+
+@pytest.fixture(scope='session', autouse=True)
+def canup(canbus_bitrate):
+    lager.can_up([0], canbus_bitrate)
+    yield
+    lager.can_down([0])
 
 @pytest.fixture(scope='session')
-def canbus(canbus_type, canbus_interface):
-    yield can.interface.Bus(bustype=canbus_type, channel=canbus_interface, bitrate=500000, receive_own_messages=True)
+def can0(canbus_type, canbus_bitrate):
+    bus = can.interface.Bus(bustype=canbus_type, channel='can0', bitrate=canbus_bitrate, receive_own_messages=True)
+    yield bus
+    bus.shutdown()
+
+@pytest.fixture(scope='session')
+def can1(canbus_type, canbus_bitrate):
+    bus = can.interface.Bus(bustype=canbus_type, channel='can1', bitrate=canbus_bitrate, receive_own_messages=True)
+    yield bus
+    bus.shutdown()
+
+@pytest.fixture(scope='session')
+def can2(canbus_type, canbus_bitrate):
+    bus = can.interface.Bus(bustype=canbus_type, channel='can2', bitrate=canbus_bitrate, receive_own_messages=True)
+    yield bus
+    bus.shutdown()
+
+@pytest.fixture(scope='session')
+def can3(canbus_type, canbus_bitrate):
+    bus = can.interface.Bus(bustype=canbus_type, channel='can3', bitrate=canbus_bitrate, receive_own_messages=True)
+    yield bus
+    bus.shutdown()
--- tests/test_can.py
+++ tests/test_can.py
@@ -1,5 +1,7 @@
+import asyncio
 import pytest
 import can
+
 
 @pytest.mark.usefixtures("flash_bootloader")
 @pytest.mark.incremental
@@ -8,11 +10,20 @@ class TestCan:
     def setup_class(cls):
         pass
 
-    def test_send_receive(self, dbc, canbus):
+    @pytest.mark.asyncio
+    async def test_some_asyncio_code(self, can0, event_loop):
+        reader = can.AsyncBufferedReader()
+        notifier = can.Notifier(can0, [reader], loop=event_loop)
+        can0.send(can.Message(arbitration_id=1))
+        msg = await reader.get_message()
+        assert msg.arbitration_id == 1
+        notifier.stop()
+
+    def test_send_receive(self, dbc, can0):
         message = dbc.get_message_by_name('PDU_Info')
         data = message.encode({'Channel': 'A', 'Cycle': 2, 'SlotID': 3, 'UBIT': 1})
-        canbus.send(can.Message(arbitration_id=message.frame_id, data=data))
-        received_message = canbus.recv(1)
+        can0.send(can.Message(arbitration_id=message.frame_id, data=data))
+        received_message = can0.recv(1)
         assert received_message is not None
         parsed = dbc.decode_message(received_message.arbitration_id, received_message.data)
         assert parsed['Channel'] == 'A'
